d79db87eae76a9700e52e6c2c14770c839d6da04
md-danish-lg <dk665226@gmail.com>
2026-07-17 10:49:55 +0500

    fix equals and hashcode methods to use only id

--- backend/src/main/java/com/danish/backend/user/User.java
+++ backend/src/main/java/com/danish/backend/user/User.java
@@ -99,11 +99,11 @@ public class User implements UserDetails {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(passwordHash, user.passwordHash) && Objects.equals(createdAt, user.createdAt);
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, email, passwordHash, createdAt);
+        return Objects.hashCode(id);
     }
 }
