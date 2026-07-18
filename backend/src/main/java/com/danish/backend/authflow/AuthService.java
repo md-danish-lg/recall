fad8739d4d3fe5831c65eae249034be75b5601e8
md-danish-lg <dk665226@gmail.com>
2026-07-18 11:04:46 +0500

    create method to find the user when logging in and verify details to return a token

--- backend/src/main/java/com/danish/backend/authflow/AuthService.java
+++ backend/src/main/java/com/danish/backend/authflow/AuthService.java
@@ -7,15 +7,20 @@ import jakarta.validation.Valid;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
     public void addNewUser(@Valid RegisterRequest registerRequest) {
@@ -31,6 +36,29 @@ public class AuthService {
         user.setPasswordHash(passwordHash);
 
         userRepository.save(user);
+
+
+
+    }
+
+    public LoginResponse findUser(LoginRequest loginRequest){
+
+        User user = userRepository.findUserByEmail(loginRequest.getEmail()).orElseThrow(()->
+                new InvalidCredentialException("Invalid Email or Password"));
+
+        String passwordHash = user.getPasswordHash();
+
+        boolean passMatches = passwordEncoder.matches(loginRequest.getPassword(), passwordHash);
+
+
+        if(!passMatches){
+            throw new InvalidCredentialException("Invalid Email or Password");
+        }
+
+        String token = jwtService.generateToken(user.getEmail());
+
+        return new LoginResponse(token);
+
 
 
 
