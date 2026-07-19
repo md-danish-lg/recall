286d74ac1ac53efc907671fc2648344cabc2cf95
md-danish-lg <dk665226@gmail.com>
2026-07-19 23:17:31 +0500

    create invalid extensions exception and handle it

---
+++ backend/src/main/java/com/danish/backend/documents/InvalidExtensionException.java
@@ -0,0 +1,7 @@
+package com.danish.backend.documents;
+
+public class InvalidExtensionException extends RuntimeException {
+    public InvalidExtensionException(String message) {
+        super(message);
+    }
+}
