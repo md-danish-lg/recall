fe84baabf6c49ad2e4fa6c4992864c617e67d901
md-danish-lg <dk665226@gmail.com>
2026-07-19 14:28:18 +0500

    create new exception and handle it

--- backend/src/main/java/com/danish/backend/GlobalExceptionHandler.java
+++ backend/src/main/java/com/danish/backend/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.danish.backend;
 
 
 import com.danish.backend.authflow.InvalidCredentialException;
+import com.danish.backend.notes.NoteNotFoundException;
 import com.danish.backend.user.EmailAlreadyExistsException;
 import com.danish.backend.user.UsernameNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -26,6 +27,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidCredentialException.class)
     public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialException ex){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoteNotFoundException.class)
+    public ResponseEntity<String> handleNoteNotFound(NoteNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
 
